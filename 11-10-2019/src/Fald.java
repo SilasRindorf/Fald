e95e1d20a79cd9281bdf7efdf9fdfc94b6525288
Silas Rindorf <s195474@student.dtu.dk>
2019-10-11 10:49:19 +0200

    Initial commit

---
+++ 11-10-2019/src/Fald.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Fald {
+    static double G = 9.80665;
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Indtast højde i meter: ");
+        if (scan.hasNextDouble() || scan.hasNextInt()) {
+            double h = scan.nextDouble();
+            if (h < 0)
+                System.out.println("Invalid højde");
+            else {
+                double tid = Math.sqrt((2 * h) / G);
+                double v = G * tid;
+                System.out.println("Tid for fald: " + tid + " sekunder");
+                System.out.println("Hastighed i slut " + v + " meter/sekunder");
+            }
+            System.out.println("Ikke tal");
+        }
+    }
+}
